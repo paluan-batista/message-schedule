e5b7b881519274e7d3d7ef0729b2e568ffae94e5
Paluan Batista <48101122+paluan-batista@users.noreply.github.com>
2022-02-08 20:26:57 -0300

    Add files via upload

---
+++ src/main/java/br/com/message/schedule/MessageScheduleApplication.java
@@ -0,0 +1,13 @@
+package br.com.message.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/message/schedule/MessageScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.message.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
